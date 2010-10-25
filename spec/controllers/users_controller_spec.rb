19986a824bd30786affaea98d6883c7435d8151b
Laurent Curau <shoops@gmail.com>
2010-10-26 01:58:01 +0200

    Added a sidebar and styling

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -18,6 +18,27 @@ describe UsersController do
       get :show, :id  => @user
       assigns(:user).should == @user 
     end
+    
+    it "should have the right title" do
+      get :show, :id  => @user
+      response.should have_selector('title', :content => @user.name)
+    end
+    
+    it "should have the user's name" do
+      get :show, :id  => @user
+      response.should have_selector('h1', :content => @user.name)
+    end
+    
+    it "should have a profile image" do
+      get :show, :id  => @user
+      response.should have_selector('h1>img', :class => "gravatar")
+    end
+    
+    it "should have the right URL" do
+      get :show, :id  => @user
+      response.should have_selector('td>a', :content  => user_path(@user),
+                                            :href     => user_path(@user))
+    end
   end
 
   describe "GET 'new'" do
